fa743dbab1628bd87be6cd2a46fbfb6f77129941
shubakarkusumba <123712104+shubakarkusumba@users.noreply.github.com>
2023-01-27 10:33:44 +0530

    added

---
+++ aritmatic operators.py
@@ -0,0 +1,23 @@
+
+print(10+3) # sum
+print(10*3) #multiply
+print(10/3) # division_float
+print(10//3) # division_int
+print(10**3) #power
+x=10
+print(x+3)
+x+=3
+print(x)
+x-=5
+print(x)
+price=input('price=')
+print(int(price)+3)
+p=input('price=')
+print(int(p)+3)
+y=-2.6
+print(round(y))
+print(abs(y))
+import math
+z=2.6
+print(math.ceil(z))
+print(math.floor(z))
---
+++ comparision operator_1.py
@@ -0,0 +1,8 @@
+temperature=input('temp=')
+if int(temperature)>30:
+    print('hot day')
+elif int(temperature)==30:
+    print('normal day')
+else:
+    print("cool day")
+
